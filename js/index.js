f4fc8ad7222c046d3b1643f26bac2f28f83548fc
vpham88 <93295690+vpham88@users.noreply.github.com>
2022-04-11 16:57:27 -0400

    Added HW 17, 18, 19
    Added HW 17, 18, and 19 to this homework to link the case study

---
+++ js/index.js
@@ -0,0 +1,33 @@
+console.log("Your index.js file is loaded correctly");
+
+$(".letsRole").mouseover(function(){
+    $("#letsRoleDes").css("display", "block");
+});
+
+$(".letsRole").mouseleave(function(){
+    $("#letsRoleDes").css("display", "none");
+});
+
+$(".doeWebsite").mouseover(function(){
+    $("#doeWebsiteDes").css("display", "block");
+});
+
+$(".doeWebsite").mouseleave(function(){
+    $("#doeWebsiteDes").css("display", "none");
+});
+
+$(".hartWebsite").mouseover(function(){
+    $("#hartWebsiteDes").css("display", "block");
+});
+
+$(".hartWebsite").mouseleave(function(){
+    $("#hartWebsiteDes").css("display", "none");
+});
+
+$(".placeHolder").mouseover(function(){
+    $("#placeHolderDes").css("display", "block");
+});
+
+$(".placeHolder").mouseleave(function(){
+    $("#placeHolderDes").css("display", "none");
+});
